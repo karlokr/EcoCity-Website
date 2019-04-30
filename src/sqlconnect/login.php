f81f31bc42909bde50bb7237cfb04f298295fb39
Karlo Krakan <karlo@krakan.ca>
2019-04-30 14:21:06 -0700

    php scripts for saving and loading users to sql db

---
+++ src/sqlconnect/login.php
@@ -0,0 +1,38 @@
+<?php
+	$con = mysqli_connect('localhost', 'root', '', 'ecocity');
+
+	//check that connection happened
+	if (mysqli_connect_errno()){
+		echo "1: Connection failed."; //error code #1 = connection failed
+		exit();
+	}
+	
+	$username = mysqli_real_escape_string($con, $_POST["name"]);
+	$password = mysqli_real_escape_string($con, $_POST["password"]);
+
+	//check if name exists
+	$namecheckquery = "SELECT username, salt, hash, score FROM users WHERE username='" . $username . "';";
+	
+	$namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed"); //error code #2 = name check query failed
+
+	if (mysqli_num_rows($namecheck) != 1) {
+		echo "5: Either no user with name or more than one"; //error code #3 = Either no user with name or more than one
+		exit();
+	}
+	
+	//get login info from query
+	$existinginfo = mysqli_fetch_assoc($namecheck);
+	$salt = $existinginfo["salt"];
+	$hash = $existinginfo["hash"];
+	
+	$loginhash = crypt($password, $salt);
+	
+	if ($hash != $loginhash) {
+		echo "6: Incorrect password."; //error code #6 = password does not hash to match table
+		exit();
+	}
+	
+	echo "0\t" . $existinginfo["score"];
+
+
+?>
---
+++ src/sqlconnect/register.php
@@ -0,0 +1,32 @@
+<?php
+	$con = mysqli_connect('localhost', 'root', '', 'ecocity');
+
+	//check that connection happened
+	if (mysqli_connect_errno()){
+		echo "1: Connection failed."; //error code #1 = connection failed
+		exit();
+	}
+	
+	$username = mysqli_real_escape_string($con, $_POST["name"]);
+	$password = mysqli_real_escape_string($con, $_POST["password"]);
+
+	//check if name exists
+	$namecheckquery = "SELECT username FROM users WHERE username='" . $username . "';";
+	
+	$namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed"); //error code #2 = name check query failed
+
+	if (mysqli_num_rows($namecheck) > 0) {
+		echo "3: Name already exists"; //error code #3 = name already exists cannot register
+		exit();
+	}
+
+	//add user to the table
+	$salt = "\$5\$rounds=5000\$" . "015%*@gfemjy" . $username . "\$";
+	$hash = crypt($password, $salt);
+	
+	$insertuserquery = "INSERT INTO users (username, hash, salt) VALUES ('" . $username . "', '" . $hash . "', '" . $salt . "');";
+
+	mysqli_query($con, $insertuserquery) or die("4: Insert player query failed"); //error code #4 = insert query failed
+		
+	echo("0");
+?>
