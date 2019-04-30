5d4ca3f2f6bd1c1a2ed4757dc23439107df25569
Karlo Krakan <karlo@krakan.ca>
2019-04-30 14:34:09 -0700

    worthless commit

--- src/server.js
+++ src/server.js
@@ -5,9 +5,20 @@ const fs = require("fs");
 app.use("/css", express.static("css"));
 app.use("/js", express.static("js"));
 app.use("/resources", express.static("resources"));
+app.use("/sqlconnect", express.static("sqlconnect"));
 
 app.get("/", function(req, res) {
 	let doc = fs.readFileSync("index.html", "utf-8");
+	res.send(doc.serialize);
+});
+
+app.get("/sqlconnect/register.php", function(req, res) {
+	let doc = fs.readFileSync("sqlconnect/register.php", "utf-8");
+	res.send(doc.serialize);
+});
+
+app.get("/sqlconnect/login.php", function(req, res) {
+	let doc = fs.readFileSync("sqlconnect/login.php", "utf-8");
 	res.send(doc.serialize);
 });
 
