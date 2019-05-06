bb2a0973741b18b1cdf03c614906c87fb0936f7f
Karlo Krakan <karlo@krakan.ca>
2019-05-05 19:06:32 -0700

    updated leaderboard scrpit

--- sqlconnect/leaderboard.php
+++ sqlconnect/leaderboard.php
@@ -6,10 +6,19 @@
 		echo "1: Connection failed."; //error code #1 = connection failed
 		exit();
 	}
+    
+    $pageno = mysqli_real_escape_string($con, $_POST["pageno"]);
+	$gamemode = mysqli_real_escape_string($con, $_POST["game_mode"]);
+	$level = mysqli_real_escape_string($con, $_POST["level"]);
 	
-	$updatequery = "SELECT * FROM (SELECT username, ecoscore FROM savegames ORDER BY ecoscore DESC) WHERE ROWNUM < 10;";
+	$leaderboardquery = "SELECT username, ecoscore FROM savegames ORDER BY ecoscore DESC WHERE game_mode = '" . $game_mode . "', level = '" . $level . "';";
 	
-	$leaderboard = mysqli_query($con, $updatequery) or die("9: Leaderboard query failed");
-		
-	echo("0" . $leaderboard);
+	$leaderboard = mysqli_query($con, $leaderboardquery) or die("9: Leaderboard query failed");
+	
+	$rows = array();
+	while ($r = mysqli_fetch_assoc($leaderboard)) {
+		$rows[] = $r;
+	}
+	
+	echo("0" . "\t" . $leaderboard);
 ?>
