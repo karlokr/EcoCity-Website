5130f697b757162191d441aff9df867430bffa34
Karlo Krakan <karlo@krakan.ca>
2019-05-22 15:25:49 -0700

    fixed saving ecoscore

--- sqlconnect/savegame.php
+++ sqlconnect/savegame.php
@@ -9,10 +9,11 @@
 	
 	$username = mysqli_real_escape_string($con, $_POST["name"]);
 	$id = mysqli_real_escape_string($con, $_POST["id"]);
+        $level = mysqli_real_escape_string($con, $_POST["level"]);
 	$ecoscore = mysqli_real_escape_string($con, $_POST["ecoscore"]);
 	$savedata = mysqli_real_escape_string($con, $_POST["savedata"]);
 
-	$insertsavequery = "INSERT INTO savegames (id, ecoscore, savedata) VALUES ('" . $id . "', '" . $ecoscore . "', '" . $savedata . "');";
+	$insertsavequery = "INSERT INTO savegames (id, username, level, ecoscore, game_data) VALUES ('" . $id . "', '" . $username . "', '" . $level . "', '" . $ecoscore . "', '" . $savedata . "');";
 
 	mysqli_query($con, $insertsavequery) or die("7: Add save game failed"); //error code #4 = insert query failed
 		
