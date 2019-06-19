c74e8deae3d2b68a695e72a07488577c3751f943
Karlo Krakan <karlo@krakan.ca>
2019-06-19 07:43:14 -0700

    fixed autoplay AGAIN

--- js/script.js
+++ js/script.js
@@ -39,94 +39,94 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
-})(jQuery); // End of use strict
-
-// Hides top bar when "play" is clicked.
-function barHide() {
-  let navBar = document.getElementById("mainNav");
-  navBar.style.display = 'none';
-}
-
-
-function goToTeam() {
-  let navBar = document.getElementById("team");
-  navBar.style.display = 'block';
-}
-
-// Restores the top bar when a user scrolls up.
-var position = $(window).scrollTop();
-$(window).scroll(function () {
-  var scroll = $(window).scrollTop();
-  if (scroll < position) {
-    $("#mainNav").css("display", "unset");
-
+  // Hides top bar when "play" is clicked.
+  function barHide() {
+    let navBar = document.getElementById("mainNav");
+    navBar.style.display = 'none';
   }
-  position = scroll;
-});
 
-// Properly scrolls a user down to the Unity pane when "play" is clicked
-$("#play").click(function () {
-  $("#unityDiv").append('<div id="unityContainer" style="width: 100%; height: 100vh; margin: auto;"></div>');
-  $("body").css("overflow", "hidden");
-  var unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json", {
-    onProgress: UnityProgress
-  });
-  let unityPane = $("#unityContainer");
-  $('html,body').animate({
-    scrollTop: unityPane.offset().top
-  }, 'slow');
-  unityPane = document.getElementById('unityContainer');
-  unityPane.addEventListener('scroll', function (e) {
-    e.preventDefault();
-  }, false);
-});
 
-$("#playbutton").click(function () {
-  $("#unityDiv").append('<div id="unityContainer" style="width: 100%; height: 100vh; margin: auto;"></div>');
-  $("body").css("overflow", "hidden");
-  var unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json", {
-    onProgress: UnityProgress
-  });
-  let unityPane = $("#unityContainer");
-  $('html,body').animate({
-    scrollTop: unityPane.offset().top
-  }, 'slow');
-  unityPane = document.getElementById('unityContainer');
-  unityPane.addEventListener('scroll', function (e) {
-    e.preventDefault();
-  }, false);
-});
+  function goToTeam() {
+    let navBar = document.getElementById("team");
+    navBar.style.display = 'block';
+  }
 
-$("#ourteam").click(function () {
-  $("#team").css("display", "block");
-  let unityPane = $("#team");
-  $('html,body').animate({
-    scrollTop: unityPane.offset().top
-  }, 'slow');
-});
+  // Restores the top bar when a user scrolls up.
+  var position = $(window).scrollTop();
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll < position) {
+      $("#mainNav").css("display", "unset");
 
-$(window).on("scroll", function () {
-  $('video').each(function () {
-    if ($(this).is(":in-viewport")) {
-      console.log("Chrome debug: video in viewport")
-      playVideo($(this)[0]);
-    } else {
-      $(this)[0].pause();
     }
+    position = scroll;
   });
-});
 
-function playVideo(video) {
-  var playPromise = video.play();
+  // Properly scrolls a user down to the Unity pane when "play" is clicked
+  $("#play").click(function () {
+    $("#unityDiv").append('<div id="unityContainer" style="width: 100%; height: 100vh; margin: auto;"></div>');
+    $("body").css("overflow", "hidden");
+    var unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json", {
+      onProgress: UnityProgress
+    });
+    let unityPane = $("#unityContainer");
+    $('html,body').animate({
+      scrollTop: unityPane.offset().top
+    }, 'slow');
+    unityPane = document.getElementById('unityContainer');
+    unityPane.addEventListener('scroll', function (e) {
+      e.preventDefault();
+    }, false);
+  });
 
-  if (playPromise !== undefined) {
-    playPromise.then(_ => {
-        // Automatic playback started!
-        // Show playing UI.
-      })
-      .catch(error => {
-        // Auto-play was prevented
-        // Show paused UI.
-      });
+  $("#playbutton").click(function () {
+    $("#unityDiv").append('<div id="unityContainer" style="width: 100%; height: 100vh; margin: auto;"></div>');
+    $("body").css("overflow", "hidden");
+    var unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.json", {
+      onProgress: UnityProgress
+    });
+    let unityPane = $("#unityContainer");
+    $('html,body').animate({
+      scrollTop: unityPane.offset().top
+    }, 'slow');
+    unityPane = document.getElementById('unityContainer');
+    unityPane.addEventListener('scroll', function (e) {
+      e.preventDefault();
+    }, false);
+  });
+
+  $("#ourteam").click(function () {
+    $("#team").css("display", "block");
+    let unityPane = $("#team");
+    $('html,body').animate({
+      scrollTop: unityPane.offset().top
+    }, 'slow');
+  });
+
+  $(window).on("scroll", function () {
+    $('video').each(function () {
+      if ($(this).is(":in-viewport")) {
+        console.log("Chrome debug: video in viewport")
+        playVideo($(this)[0]);
+      } else {
+        $(this)[0].pause();
+      }
+    });
+  });
+
+  function playVideo(video) {
+    var playPromise = video.play();
+
+    if (playPromise !== undefined) {
+      playPromise.then(_ => {
+          // Automatic playback started!
+          // Show playing UI.
+        })
+        .catch(error => {
+          // Auto-play was prevented
+          // Show paused UI.
+        });
+    }
   }
-}
+
+})(jQuery); // End of use strict
